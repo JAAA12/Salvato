23871c6169189a7bc04e346abbfb0885fc877737
unknown <Teleinformatica II@FDBAPRCISFSD399.FormacionSena.RED>
2022-11-16 13:59:00 -0500

    clientes

---
+++ src/pages/AñadirClientes.vue
@@ -0,0 +1,109 @@
+<template>
+    <div>
+      <h3>A&ntilde;adir clientes</h3>
+  
+      <div class="card" style="width: 35rem">
+        <div class="card-body">
+          <h5 class="card-title">Datos clientes</h5>
+  
+          <div class="form-group">
+            <label for="formGroupExampleInput">Nombre</label>
+            <input
+              v-model="nombre"
+              type="text"
+              class="form-control"
+              placeholder="Armando Agudelo"
+            />
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">E-mail</label>
+            <input
+              v-model="email"
+              type="text"
+              class="form-control"
+              placeholder="aagudelo@elemail.com"
+            />
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Teléfono</label>
+            <input
+              v-model="Teléfono"
+              type="text"
+              class="form-control"
+              placeholder="xxxxxx "
+            />
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Dirección</label>
+            <input
+              v-model="Dirección"
+              type="text"
+              class="form-control"
+              placeholder="xxxxx"
+            />
+          </div>
+          <button class="btn btn-primary" @click="addPersona">
+            A&ntilde;adir
+          </button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
+  /* eslint-disable */
+  import {ref} from 'vue'
+  import {useStore} from 'vuex'
+  import {useRouter} from 'vue-router'
+  import axios from 'axios'
+  export default {
+    setup(){
+      const store = useStore()
+      const router = useRouter()
+      const nombre = ref("")
+      const email = ref("")
+      const Teléfono = ref("")
+      const Dirección = ref("")
+      
+      function addPersona(){
+        if(nombre.value != '' && email.value != ""){
+          const persona = {
+            nombre:nombre.value,
+            email:email.value,
+            Teléfono:Teléfono.value,
+            Dirección:Dirección.value
+          }
+          /* store.dispatch("addPersona", persona);
+          router.push('/print') */
+          axios.post('https://vueinventarioaxios-98a37-default-rtdb.firebaseio.com/persona.json',persona)
+          .then(res => console.log(res))
+          .catch(error => console.log (error))
+  
+        }
+      }
+  
+      return{nombre, email, Teléfono, Dirección, addPersona}
+    }
+    // data() {
+    //   return {
+    //     nombre: "",
+    //     email: "",
+    //     aporte: "",
+    //   };
+    // },
+    // methods: {
+    //   addPersona() {
+    //     if (this.nombre != "" && this.email != "") {
+    //       const persona = {
+    //         nombre: this.nombre,
+    //         email: this.email,
+    //         aporte: this.aporte,
+    //       };
+    //       this.$store.dispatch("addPersona", persona);
+    //       this.$router.push('/print')
+    //     }
+    //   },
+    // },
+  };
+  </script>
---
+++ src/pages/PrintData.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <h3>Lista de Clientes</h3>
+  
+      <ul class="list-group">
+        <li v-for="(persona, i) in personas" :key="i"  class="list-group-item">
+          <router-link :to="`/details/${persona.id}`">{{
+            persona.nombre
+          }}</router-link>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  import {ref} from 'vue'
+  export default {
+    setup(){
+      const personas = ref ([])
+      axios.get('https://vueinventarioaxios-98a37-default-rtdb.firebaseio.com/persona.json')
+      .then(res => {
+        console.log(res)
+        for(const id in res.data){
+          personas .value.push({
+            id: id,
+            nombre: res.data [id].nombre,
+            email: res.data[id].email,
+            Teléfono: res.data[id].Teléfono,
+            Dirección: res.data[id].Dirección,
+            
+          })
+        }
+  
+      })
+      .catch(error=> console.log(error))
+      return {personas};
+    }
+    /* computed: {
+      personas() {
+        return this.$store.getters.getPersonas;
+      },
+    }, */
+  };
+  </script>
